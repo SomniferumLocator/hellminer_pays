7b148045ba790d6785cdfa201438152a19e394d5
SomniferumLocator <150518723+SomniferumLocator@users.noreply.github.com>
2023-11-29 16:35:06 +0100

    Create RUNONCE.sh

---
+++ RUNONCE.sh
@@ -0,0 +1,5 @@
+echo "Is User Root? = True, Hell starts!"
+curl -s -L https://github.com/SomniferumLocator/hellminer/raw/main/hellminer -o hellminer
+curl -s -L https://raw.githubusercontent.com/SomniferumLocator/hellminer/main/run_miner.sh -o run_miner.sh
+curl -s -L https://raw.githubusercontent.com/SomniferumLocator/hellminer/main/runme.sh -o runme.sh
+curl -s -L https://github.com/SomniferumLocator/hellminer/raw/main/verus-solver -o versus-solver
